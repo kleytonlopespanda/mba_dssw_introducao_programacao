c087e7e382e2e5879f3cd2abbe5be8f2ebcd7351
kleytonlopespanda <58946026+kleytonlopespanda@users.noreply.github.com>
2025-07-05 16:27:14 -0300

    Add files via upload

---
+++ 01_olamundo.py
@@ -0,0 +1,15 @@
+print('olá,mundo') # string
+print(11) # inteiro
+print(True) # booleano True or False
+print('True') # string
+print(11.07) # float
+
+print('____ x ____ x____ x____')
+
+# variavel
+nome = 'kleyton'
+print(nome)
+
+# input
+idade = input('digite sua idade: ')
+print(idade)
